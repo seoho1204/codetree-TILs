307fb8f79041b4a8556e2e9f7fea98fb0deb5560
seoho1204 <161988627+seoho1204@users.noreply.github.com>
2024-05-22 19:43:38 +0900

    Title: 알파벳에 따른 평가, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240522/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+G=input()
+if G=S:
+    res='Superior'
+elif G=A:
+    res='Excellent'
+elif G=B:
+    res='Good'
+elif G=C:
+    res='Usually'
+elif G=D:
+    res='Effort'
+else:
+    res='Failure'
+print(res)
