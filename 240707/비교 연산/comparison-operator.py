58c7e8fd23cfecbd8d34069fd12b9805f0ade004
seoho1204 <161988627+seoho1204@users.noreply.github.com>
2024-07-07 20:45:55 +0900

    Title: 비교 연산, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240707/비교 연산/comparison-operator.py
@@ -0,0 +1,33 @@
+a,b=input().split(' ')
+a=int(a)
+b=int(b)
+if b<=a:
+    res=1
+else:
+    res=0
+print(res)
+if b<a:
+    res=1
+else:
+    res=0
+print(res)
+if a<=b:
+    res=1
+else:
+    res=0
+print(res)
+if a<b:
+    res=1
+else:
+    res=0
+print(res)
+if a==b:
+    res=1
+else:
+    res=0
+print(res)
+if a!=b:
+    res=1
+else:
+    res=0
+print(res)
